50dc9efe4de870006cb49c1d3585cb6924362c03
Brian Faust <faustbrian@users.noreply.github.com>
2016-12-04 13:54:52 +0200

    Apply fixes from StyleCI (#7)
    [ci skip] [skip ci]

--- src/Console/SeedCurrencies.php
+++ src/Console/SeedCurrencies.php
@@ -11,11 +11,20 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Countries.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Countries\Console;
 
 use Illuminate\Console\Command;
-use NumberFormatter;
 use Illuminate\Database\Eloquent\Model;
+use NumberFormatter;
 
 class SeedCurrencies extends Command
 {
--- src/Models/Country.php
+++ src/Models/Country.php
@@ -11,12 +11,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Countries.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Countries\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Country extends Model
 {
